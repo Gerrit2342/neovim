ada82ed813545416a47c634f3711715a8763b458
Gerrit Albert <galbert@physik.rwth-aachen.de>
2023-03-13 09:39:34 +0100

    Grüße in all.lua hinzugefügt

--- LuaSnip/all.lua
+++ LuaSnip/all.lua
@@ -1,16 +1,42 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
 
--- Place this in ${HOME}/.config/nvim/LuaSnip/all.lua
 return {
-  -- A snippet that expands the trigger "hi" into the string "Hello, world!".
-  require("luasnip").snippet(
-    { trig = "sehr" },
-    { t("Sehr geehrter Herr!") }
-  ),
+-- Examples of complete snippets using fmt and fmta
 
-  -- To return multiple snippets, use one `return` statement per snippet file
-  -- and return a table of Lua snippets.
-  require("luasnip").snippet(
-    { trig = "mfg" },
-    { t("Mit freundlichen Grüßen") }
-  ),
+s({trig="mfg", snippetType="autosnippet"},
+  fmta(
+	[[
+	Mit freundlichen Grüßen
+	Gerrit Albert
+	]],{}
+      )
+),
+
+s({trig="vg", snippetType="autosnippet"},
+  fmta(
+	[[
+	Viele Grüße
+	Gerrit Albert
+	]],{}
+      )
+),
+
+s({trig="lg", snippetType="autosnippet"},
+  fmta(
+	[[
+	Liebe Grüße
+	Gerrit
+	]],{}
+      )
+)
+
 }
